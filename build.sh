3e0392250170e513ee6891b0778fb47b661cc14b
thoryo <53646847+miaoyehao@users.noreply.github.com>
2025-02-16 17:14:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ git clone https://gitlab.com/mmqmemm/android_kernel_oppo_msm8937 -b staging/line
 PATH=/home/miao/clang-r416183b1/bin:$PATH export 
 PATH=/home/miao/android-ndk-r16-beta1/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH export 
 PATH=/home/miao/android-ndk-r16-beta1/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
-make -j$(nproc) ARCH=arm64  CC=clang LD=ld.lld HOSTCC=clang HOSTLD=ld.lld O=out KCFLAGS+=-Wno-error merge_hi6250_defconfig all
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabihf- CC=clang HOSTCC=clang  O=out KCFLAGS+=-Wno-error lineageos_A57_defconfig all
